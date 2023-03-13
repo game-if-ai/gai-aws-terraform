7da7927a95d14a15f98641433a5eb80a91577ed5
aaronshiel <root@DESKTOP-IOBK01F.localdomain>
2023-03-12 19:20:22 -0700

    gameifai frontend terraform

---
+++ terraform/modules/frontend-gameifai/variables.tf
@@ -0,0 +1,35 @@
+variable "eb_env_namespace" {
+  type        = string
+  description = "Namespace, which could be your organization name, e.g. 'eg' or 'cp'"
+}
+
+variable "aws_acm_certificate_domain" {
+  type        = string
+  description = "domain name to find ssl certificate"
+}
+
+variable "site_domain_name" {
+  type        = string
+  description = "the public domain name for this site, e.g. gameifai.org"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "aws_route53_zone_name" {
+  type        = string
+  description = "name to find aws route53 zone, e.g. gameifai.org."
+}
+
+variable "eb_env_name" {
+  type        = string
+  description = "Solution name, e.g. 'app' or 'cluster'"
+}
+
+variable "eb_env_stage" {
+  type        = string
+  description = "Stage, e.g. 'prod', 'staging', 'dev', or 'test'"
+  default     = "dev"
+}
